ecb6e5b5ba59af35568919d33e6a10b0efc397dd
unknown <merrill.huang@gmail.com>
2023-10-22 15:55:54 -0500

    create SpringBoot project

---
+++ src/main/java/com/example/simplechatbackend/SimpleChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplechatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleChatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplechatbackend/SimpleChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplechatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
