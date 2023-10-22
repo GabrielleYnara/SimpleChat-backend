62a849ba21046488e051fed98834b828e6ac6a16
unknown <merrill.huang@gmail.com>
2023-10-22 18:06:12 -0500

    set up User model

--- src/main/java/com/example/simplechatbackend/model/User.java
+++ src/main/java/com/example/simplechatbackend/model/User.java
@@ -1,4 +1,77 @@
 package com.example.simplechatbackend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = 'users')
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    @LazyCollection(LazyCollectionOption.TRUE)
+    private Set<Chat> chatList;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Chat> getChatList() {
+        return chatList;
+    }
+
+    public void setChatList(Set<Chat> chatList) {
+        this.chatList = chatList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
