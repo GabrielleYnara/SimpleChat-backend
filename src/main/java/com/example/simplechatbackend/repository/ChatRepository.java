e67ca4ddf9216396a77add959c77e30f395e9ca7
unknown <merrill.huang@gmail.com>
2023-10-23 09:22:21 -0500

    fill out ChatRepository

--- src/main/java/com/example/simplechatbackend/repository/ChatRepository.java
+++ src/main/java/com/example/simplechatbackend/repository/ChatRepository.java
@@ -1,4 +1,9 @@
 package com.example.simplechatbackend.repository;
 
-public interface ChatRepository {
+import com.example.simplechatbackend.model.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, Long> {
 }
