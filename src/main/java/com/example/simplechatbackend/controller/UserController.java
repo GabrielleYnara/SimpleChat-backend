e568f67d659f64a6652e3c712f4f11eb64da6272
unknown <merrill.huang@gmail.com>
2023-10-23 09:42:21 -0500

    initial setup for UserController class

--- src/main/java/com/example/simplechatbackend/controller/UserController.java
+++ src/main/java/com/example/simplechatbackend/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.example.simplechatbackend.controller;
 
+import com.example.simplechatbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/user")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
