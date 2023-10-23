bf216109beb2267d5c237070435fad51888560ea
unknown <merrill.huang@gmail.com>
2023-10-23 16:45:03 -0500

    fill out JWTUtils

--- src/main/java/com/example/simplechatbackend/security/JWTUtils.java
+++ src/main/java/com/example/simplechatbackend/security/JWTUtils.java
@@ -1,4 +1,68 @@
 package com.example.simplechatbackend.security;
 
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
 public class JWTUtils {
+
+    Logger logger  = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpMS;
+
+    /**
+     * Runs on User login, generates JWT from current logged in user.
+     * @param myUserDetails Contains the details of the current logged in user.
+     * @return A JWT
+     */
+    public String generateJwtToken(MyUserDetails myUserDetails) {
+        return Jwts.builder()
+                .setSubject(myUserDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpMS))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+    /**
+     * Runs for every request, discerns the current logged in User without requiring re-login
+     * through decoding JWT.
+     * @param token The JWT passed in from the Http request
+     * @return The username of the current logged in User.
+     */
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    /**
+     * Runs for every request. Validates that the JWT passed from the Http request is still valid.
+     * @param token JWT passed from Http request
+     * @return True if JWT is valid, false if not.
+     */
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e) {
+            logger.log(Level.SEVERE, "Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.log(Level.SEVERE, "Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.log(Level.SEVERE, "JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
 }
