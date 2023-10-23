49d40a9e1ea41cbf812fb47875dcb027f656d8d6
unknown <merrill.huang@gmail.com>
2023-10-23 16:17:17 -0500

    fill out LoginResponse class

--- src/main/java/com/example/simplechatbackend/model/response/LoginResponse.java
+++ src/main/java/com/example/simplechatbackend/model/response/LoginResponse.java
@@ -1,4 +1,18 @@
 package com.example.simplechatbackend.model.response;
 
 public class LoginResponse {
+
+    private String jwt;
+
+    public LoginResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
 }
