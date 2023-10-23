bc3a96f482c0e369749d748507b5c6a022d1ad5d
unknown <merrill.huang@gmail.com>
2023-10-23 09:21:03 -0500

    fill out RoomRepository

--- src/main/java/com/example/simplechatbackend/repository/RoomRepository.java
+++ src/main/java/com/example/simplechatbackend/repository/RoomRepository.java
@@ -1,4 +1,9 @@
 package com.example.simplechatbackend.repository;
 
-public interface RoomRepository {
+import com.example.simplechatbackend.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
