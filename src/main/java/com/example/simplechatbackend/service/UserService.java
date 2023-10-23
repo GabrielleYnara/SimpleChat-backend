cc583ecaa59b34ea24f345e29b9c136b8752916e
unknown <merrill.huang@gmail.com>
2023-10-23 09:31:49 -0500

    initial setup fpr UserService class

--- src/main/java/com/example/simplechatbackend/service/UserService.java
+++ src/main/java/com/example/simplechatbackend/service/UserService.java
@@ -1,4 +1,16 @@
 package com.example.simplechatbackend.service;
 
+import com.example.simplechatbackend.repository.ChatRepository;
+import com.example.simplechatbackend.repository.RoomRepository;
+import com.example.simplechatbackend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    private RoomRepository roomRepository;
+
+    private ChatRepository chatRepository;
 }
