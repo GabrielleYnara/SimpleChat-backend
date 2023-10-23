52d1b25d2a6021f3ffa1f14ca8d1ff646d59aaa3
unknown <merrill.huang@gmail.com>
2023-10-23 16:15:15 -0500

    fill out LoginRequest

--- src/main/java/com/example/simplechatbackend/model/request/LoginRequest.java
+++ src/main/java/com/example/simplechatbackend/model/request/LoginRequest.java
@@ -1,4 +1,16 @@
 package com.example.simplechatbackend.model.request;
 
 public class LoginRequest {
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
