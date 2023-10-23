3ec4b2e959e6b78868cb0d5a6888893cb317e46d
unknown <merrill.huang@gmail.com>
2023-10-23 09:37:14 -0500

    initial setup for AuthController class

--- src/main/java/com/example/simplechatbackend/controller/AuthController.java
+++ src/main/java/com/example/simplechatbackend/controller/AuthController.java
@@ -1,4 +1,18 @@
 package com.example.simplechatbackend.controller;
 
+import com.example.simplechatbackend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth")     //localhost:8081/auth
 public class AuthController {
+
+    private AuthService authService;
+
+    @Autowired
+    public void setAuthService(AuthService authService) {
+        this.authService = authService;
+    }
 }
