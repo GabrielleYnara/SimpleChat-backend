12c0d1b62fec72742d15edc0dd2905fb2fde87aa
unknown <merrill.huang@gmail.com>
2023-10-23 09:30:25 -0500

    initial setup for AuthService class

--- src/main/java/com/example/simplechatbackend/service/AuthService.java
+++ src/main/java/com/example/simplechatbackend/service/AuthService.java
@@ -1,4 +1,10 @@
 package com.example.simplechatbackend.service;
 
+import com.example.simplechatbackend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    private UserRepository userRepository;
 }
