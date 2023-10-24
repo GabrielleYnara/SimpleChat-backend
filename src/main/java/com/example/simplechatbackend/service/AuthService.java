ad844e4d02e9e8e0a6aec3225d6c68e5dac67512
unknown <merrill.huang@gmail.com>
2023-10-24 15:10:33 -0500

    add necessary utility classes for logging and registering in AuthService

--- src/main/java/com/example/simplechatbackend/service/AuthService.java
+++ src/main/java/com/example/simplechatbackend/service/AuthService.java
@@ -1,10 +1,30 @@
 package com.example.simplechatbackend.service;
 
 import com.example.simplechatbackend.repository.UserRepository;
+import com.example.simplechatbackend.security.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JWTUtils jwtUtils;
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+    }
 }
