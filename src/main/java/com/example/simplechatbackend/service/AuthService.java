8a06a1e82ea06d41159fa82ec89afa52808d8685
unknown <merrill.huang@gmail.com>
2023-10-24 15:11:59 -0500

    implement loginUser in AuthService

--- src/main/java/com/example/simplechatbackend/service/AuthService.java
+++ src/main/java/com/example/simplechatbackend/service/AuthService.java
@@ -1,12 +1,19 @@
 package com.example.simplechatbackend.service;
 
+import com.example.simplechatbackend.model.request.LoginRequest;
 import com.example.simplechatbackend.repository.UserRepository;
 import com.example.simplechatbackend.security.JWTUtils;
+import com.example.simplechatbackend.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -26,5 +33,19 @@ public class AuthService {
         this.passwordEncoder = passwordEncoder;
         this.jwtUtils = jwtUtils;
         this.authenticationManager = authenticationManager;
+    }
+
+
+    public Optional<String> loginUser(LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }
