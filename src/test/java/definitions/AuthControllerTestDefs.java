4a0fb7e4a39a2e1f26881760abcaedd622354558
unknown <merrill.huang@gmail.com>
2023-10-24 15:02:42 -0500

    add setup and properties to AuthControllerTestDefs

--- src/test/java/definitions/AuthControllerTestDefs.java
+++ src/test/java/definitions/AuthControllerTestDefs.java
@@ -1,8 +1,27 @@
 package definitions;
 
+import com.example.simplechatbackend.SimpleChatBackendApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.logging.Logger;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SimpleChatBackendApplication.class)
 public class AuthControllerTestDefs {
+
+    private static final Logger logger = Logger.getLogger(AuthControllerTestDefs.class.getName());
+
+    private static final String BASE_URL = "http://localhost:";
+
+    @LocalServerPort
+    String port;
+
+    private static Response response;
+
     @Given("There are Users in the database")
     public void thereAreUsersInTheDatabase() {
     }
