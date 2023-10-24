0340745f89c0a3a28f8320499df268f3d23a1fb0
unknown <merrill.huang@gmail.com>
2023-10-24 17:11:40 -0500

    implement createUser PostMapping

--- src/main/java/com/example/simplechatbackend/controller/AuthController.java
+++ src/main/java/com/example/simplechatbackend/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.simplechatbackend.controller;
 
+import com.example.simplechatbackend.model.User;
 import com.example.simplechatbackend.model.request.LoginRequest;
 import com.example.simplechatbackend.model.response.LoginResponse;
 import com.example.simplechatbackend.service.AuthService;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +20,8 @@ import java.util.Optional;
 public class AuthController {
 
     private AuthService authService;
+
+    static HashMap<String, Object> message = new HashMap<>();
 
     @Autowired
     public void setAuthService(AuthService authService) {
@@ -31,6 +35,20 @@ public class AuthController {
             return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body((new LoginResponse("Authentication failed")));
+        }
+    }
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        Optional<User> newUser = authService.createUser(user);
+
+        if(newUser.isPresent()) {
+            message.put("message", "Sucessfully registered new User: " + user.getUsername());
+            message.put("data", newUser);
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
+        } else {
+            message.put("message", "User with username " + user.getUsername() + " already exists.");
+            return new ResponseEntity<>(message, HttpStatus.CONFLICT);
         }
     }
 }
