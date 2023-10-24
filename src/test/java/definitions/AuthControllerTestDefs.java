ae3312c2641eaf517f1e5d8ee80ac5438b26dc31
unknown <merrill.huang@gmail.com>
2023-10-24 15:00:32 -0500

    create AuthControllerTestDefs

---
+++ src/test/java/definitions/AuthControllerTestDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class AuthControllerTestDefs {
+    @Given("There are Users in the database")
+    public void thereAreUsersInTheDatabase() {
+    }
+}
