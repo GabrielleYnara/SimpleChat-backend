818c8d5d862607c228052f1b48835c215c1645c9
unknown <merrill.huang@gmail.com>
2023-10-24 14:34:47 -0500

    add two Users as SeedData

--- src/main/java/com/example/simplechatbackend/seed/SeedData.java
+++ src/main/java/com/example/simplechatbackend/seed/SeedData.java
@@ -1,4 +1,22 @@
 package com.example.simplechatbackend.seed;
 
-public class SeedData {
+import com.example.simplechatbackend.model.User;
+import com.example.simplechatbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class SeedData implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.count() == 0) {
+            User user1 = new User("bobmyers", "password1");
+            User user2 = new User("susiemyers", "password2");
+            userRepository.save(user1);
+            userRepository.save(user2);
+        }
+    }
 }
