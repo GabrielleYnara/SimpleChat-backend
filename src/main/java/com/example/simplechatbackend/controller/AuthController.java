8f4a85589d89072bec703f2a31ed29a1166aa376
unknown <merrill.huang@gmail.com>
2023-10-24 15:08:44 -0500

    add login postMapping in AuthController

--- src/main/java/com/example/simplechatbackend/controller/AuthController.java
+++ src/main/java/com/example/simplechatbackend/controller/AuthController.java
@@ -1,9 +1,17 @@
 package com.example.simplechatbackend.controller;
 
+import com.example.simplechatbackend.model.request.LoginRequest;
+import com.example.simplechatbackend.model.response.LoginResponse;
 import com.example.simplechatbackend.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/auth")     //localhost:8081/auth
@@ -14,5 +22,15 @@ public class AuthController {
     @Autowired
     public void setAuthService(AuthService authService) {
         this.authService = authService;
+    }
+
+    @PostMapping(path = "/login") // localhost:8081/auth/login
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
+        Optional<String> jwtToken = authService.loginUser(loginRequest);
+        if (jwtToken.isPresent()) {
+            return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body((new LoginResponse("Authentication failed")));
+        }
     }
 }
