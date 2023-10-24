2174315ba8eced18ea83f0bbcdf7945ab44b280c
unknown <merrill.huang@gmail.com>
2023-10-24 14:44:12 -0500

    refactor SeedData to use PasswordEncoder

--- src/main/java/com/example/simplechatbackend/seed/SeedData.java
+++ src/main/java/com/example/simplechatbackend/seed/SeedData.java
@@ -4,18 +4,33 @@ import com.example.simplechatbackend.model.User;
 import com.example.simplechatbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SeedData implements CommandLineRunner {
 
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
     @Autowired
-    UserRepository userRepository;
+    public SeedData(@Lazy PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
         if (userRepository.count() == 0) {
-            User user1 = new User("bobmyers", "password1");
-            User user2 = new User("susiemyers", "password2");
+            User user1 = new User();
+            user1.setUsername("bobmyers");
+            user1.setPassword(passwordEncoder.encode("password1"));
             userRepository.save(user1);
+            User user2 = new User();
+            user2.setUsername("susiemyers");
+            user2.setPassword(passwordEncoder.encode("password2"));
             userRepository.save(user2);
         }
     }
