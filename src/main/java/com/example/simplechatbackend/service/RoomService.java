f43f1135766e9f5561e26ac2b795d04f319f1b50
unknown <merrill.huang@gmail.com>
2023-10-25 18:39:05 -0500

    implement getAllBooks and RoomControllerTests reciveRooms passes

--- src/main/java/com/example/simplechatbackend/service/RoomService.java
+++ src/main/java/com/example/simplechatbackend/service/RoomService.java
@@ -1,10 +1,13 @@
 package com.example.simplechatbackend.service;
 
+import com.example.simplechatbackend.model.Room;
 import com.example.simplechatbackend.repository.ChatRepository;
 import com.example.simplechatbackend.repository.RoomRepository;
 import com.example.simplechatbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomService {
@@ -14,5 +17,9 @@ public class RoomService {
     @Autowired
     public void setRoomRepository(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getAllBooks() {
+        return roomRepository.findAll();
     }
 }
