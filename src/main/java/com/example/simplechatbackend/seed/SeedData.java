5e5b0b9d9caf13e65d8ae735bdb34f8bd0477d6e
unknown <merrill.huang@gmail.com>
2023-10-25 18:11:03 -0500

    add seed Rooms to seedData

--- src/main/java/com/example/simplechatbackend/seed/SeedData.java
+++ src/main/java/com/example/simplechatbackend/seed/SeedData.java
@@ -1,5 +1,6 @@
 package com.example.simplechatbackend.seed;
 
+import com.example.simplechatbackend.model.Room;
 import com.example.simplechatbackend.model.User;
 import com.example.simplechatbackend.repository.RoomRepository;
 import com.example.simplechatbackend.repository.UserRepository;
@@ -33,10 +34,25 @@ public class SeedData implements CommandLineRunner {
             user1.setUsername("bobmyers");
             user1.setPassword(passwordEncoder.encode("password1"));
             userRepository.save(user1);
+
             User user2 = new User();
             user2.setUsername("susiemyers");
             user2.setPassword(passwordEncoder.encode("password2"));
             userRepository.save(user2);
+        }
+
+        if (roomRepository.count() == 0) {
+            Room room1 = new Room();
+            room1.setName("Dogs");
+            roomRepository.save(room1);
+
+            Room room2 = new Room();
+            room2.setName("Cats");
+            roomRepository.save(room2);
+
+            Room room3 = new Room();
+            room3.setName("Horses");
+            roomRepository.save(room3);
         }
     }
 }
