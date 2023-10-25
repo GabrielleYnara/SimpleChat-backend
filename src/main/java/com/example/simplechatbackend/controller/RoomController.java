7482dd625186367f1fc0c3fd2cf8a9021440bdc3
unknown <merrill.huang@gmail.com>
2023-10-25 18:50:53 -0500

    write createRoom Post Mapping in RoomController

--- src/main/java/com/example/simplechatbackend/controller/RoomController.java
+++ src/main/java/com/example/simplechatbackend/controller/RoomController.java
@@ -5,12 +5,11 @@ import com.example.simplechatbackend.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/rooms")  //localhost:8081/rooms
@@ -35,6 +34,19 @@ public class RoomController {
             message.put("message", "Success!");
             message.put("data", roomList);
             return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<?> createRoom(@RequestBody Room room) {
+        Optional<Room> newRoom = roomService.createRoom();
+        if(newRoom.isPresent()) {
+            message.put("message", "Successfully registered new Room: " + room.getName());
+            message.put("data", newRoom.get());
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
+        } else {
+            message.put("message", "Room with name " + room.getName() + " already exists.");
+            return new ResponseEntity<>(message, HttpStatus.CONFLICT);
         }
     }
 }
