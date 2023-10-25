097ab2edb70650486109014d0ea9c763c2a73030
unknown <merrill.huang@gmail.com>
2023-10-25 18:18:51 -0500

    inject RoomRepository into RoomService

--- src/main/java/com/example/simplechatbackend/service/RoomService.java
+++ src/main/java/com/example/simplechatbackend/service/RoomService.java
@@ -3,6 +3,7 @@ package com.example.simplechatbackend.service;
 import com.example.simplechatbackend.repository.ChatRepository;
 import com.example.simplechatbackend.repository.RoomRepository;
 import com.example.simplechatbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +11,8 @@ public class RoomService {
 
     private RoomRepository roomRepository;
 
+    @Autowired
+    public void setRoomRepository(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
 }
