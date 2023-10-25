4ed59bbc3705e119861c668a5a7d325a8afa76f5
unknown <merrill.huang@gmail.com>
2023-10-25 17:43:12 -0500

    create UserControllerTestDefs file

---
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class UserControllerTestDefs {
+    @Given("The User is logged in")
+    public void theUserIsLoggedIn() {
+    }
+}
