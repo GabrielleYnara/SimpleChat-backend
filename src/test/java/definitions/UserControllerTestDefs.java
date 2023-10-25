8c8059edf41180b6d0cd7614b1623f966e732ede
unknown <merrill.huang@gmail.com>
2023-10-25 17:45:01 -0500

    add necessary configuration to UserControllerTestDefs

--- src/test/java/definitions/UserControllerTestDefs.java
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -1,8 +1,28 @@
 package definitions;
 
+import com.example.simplechatbackend.SimpleChatBackendApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.logging.Logger;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SimpleChatBackendApplication.class)
 
 public class UserControllerTestDefs {
+
+    private static final Logger logger = Logger.getLogger(AuthControllerTestDefs.class.getName());
+
+    private static final String BASE_URL = "http://localhost:";
+
+    @LocalServerPort
+    String port;
+
+    private static Response response;
+
     @Given("The User is logged in")
     public void theUserIsLoggedIn() {
     }
