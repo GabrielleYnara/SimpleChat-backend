ee8b5447e30e8bc7f22ac41cb3eed5f1d2f2c47e
unknown <merrill.huang@gmail.com>
2023-10-25 18:24:46 -0500

    implement getAllRooms Get Mapping in RoomController

--- src/main/java/com/example/simplechatbackend/controller/RoomController.java
+++ src/main/java/com/example/simplechatbackend/controller/RoomController.java
@@ -1,18 +1,40 @@
 package com.example.simplechatbackend.controller;
 
+import com.example.simplechatbackend.model.Room;
 import com.example.simplechatbackend.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/rooms")
+@RequestMapping(path = "/rooms")  //localhost:8081/rooms
 public class RoomController {
 
     private RoomService roomService;
 
+    static HashMap<String, Object> message = new HashMap<>();
+
     @Autowired
-    public void setUserService(RoomService roomService) {
+    public void setRoomService(RoomService roomService) {
         this.roomService = roomService;
+    }
+
+    @GetMapping(path = "")  //localhost:8081/rooms
+    public ResponseEntity<?> getAllRooms() {
+        List<Room> roomList = roomService.getAllBooks();
+        if (roomList.isEmpty()) {
+            message.put("message", "No Rooms exist in the database");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "Success!");
+            message.put("data", roomList);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
     }
 }
