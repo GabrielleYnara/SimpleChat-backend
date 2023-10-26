6ad8a6e5b28685a88417be4354c5c19922763589
unknown <merrill.huang@gmail.com>
2023-10-25 22:41:42 -0500

    inject ChatRepository and JWTUtils instances into RoomService

--- src/main/java/com/example/simplechatbackend/service/RoomService.java
+++ src/main/java/com/example/simplechatbackend/service/RoomService.java
@@ -2,10 +2,12 @@ package com.example.simplechatbackend.service;
 
 import com.example.simplechatbackend.exception.InformationAlreadyExistsException;
 import com.example.simplechatbackend.exception.InformationNotFoundException;
+import com.example.simplechatbackend.model.Chat;
 import com.example.simplechatbackend.model.Room;
 import com.example.simplechatbackend.repository.ChatRepository;
 import com.example.simplechatbackend.repository.RoomRepository;
 import com.example.simplechatbackend.repository.UserRepository;
+import com.example.simplechatbackend.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +19,23 @@ public class RoomService {
 
     private RoomRepository roomRepository;
 
+    private ChatRepository chatRepository;
+
+    private JWTUtils jwtUtils;
+
     @Autowired
     public void setRoomRepository(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
+    }
+
+    @Autowired
+    public void setChatRepository(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
+
+    @Autowired
+    public void setJwtUtils(JWTUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
     }
 
     public List<Room> getAllBooks() {
